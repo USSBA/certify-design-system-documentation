528e268550b1782189e9574127056618bab3d786
Jared Cunha <jcunha@omb.eop.gov>
2017-06-28 09:40:25 -0400

    add transitions to the task panel

--- assets/javascripts/components/task_panel.js
+++ assets/javascripts/components/task_panel.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   var $task_panel_toggle = $('.task-panel-toggle'),
+      transition_class = "in-transition",
       visible_class = "visible";
 
   $task_panel_toggle.on('click', function(){
@@ -8,10 +9,17 @@ $(document).ready(function(){
     if ($(this).attr("aria-expanded") === "false") {
       $(this).attr("aria-expanded", "true");
       $target.addClass(visible_class);
+      setTimeout(function(){
+        $target.addClass(transition_class)
+      }, 20);
     }
     else {
       $(this).attr("aria-expanded", "false");
-      $target.removeClass(visible_class);
+      $target.removeClass(transition_class);
+      $target.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+      function(e) {
+        $target.removeClass(visible_class);
+      });
     }
   });
 
