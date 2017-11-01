1d7a95bc57e79308753c1d057b7fd7a162544481
Jared Cunha <jcunha@omb.eop.gov>
2017-11-01 13:54:05 -0400

    update js to be more efficient

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -15,7 +15,12 @@
   -{% include_relative components/prep-storage.js %}
   -{% include_relative components/session-storage.js %}
   -{% include_relative components/smooth_scrolling.js %}
+    -{% include_relative components/store-url-params.js %}
   -{% include_relative components/table-sort.js %}
   -{% include_relative components/task_panel.js %}
+  -{% include_relative components/filter-content.js %}
   -{% include_relative components/taskbar.js %}
   -{% include_relative components/urlparam.js %}
+
+  // Add your JS sripts here
+   -{% include_relative init.js %}
