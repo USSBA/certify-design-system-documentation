f5ce2a0b548fcad079caea194e22c4bd6ff63900
Jared Cunha <jcunha@omb.eop.gov>
2017-11-07 09:51:49 -0500

    new grid system

--- assets/javascripts/components/taskbar.js
+++ assets/javascripts/components/taskbar.js
@@ -3,9 +3,15 @@ $(document).ready(function(){
   var $taskbar_wrap = $('#taskbar-wrap');
 
   if ($taskbar_wrap.length) {
-    var elementOffset = $taskbar_wrap.offset().top,
-    taskbar_height = $taskbar_wrap.height(),
-    placeholder = '<div class="taskbar-placeholder" style="height:'+taskbar_height+'px"></div>';
+
+    var elementOffset = $taskbar_wrap.offset().top;
+
+    // This was moved to a setTimeout only becuase it was taking a long time
+    // for other things on the page to load.
+    setTimeout(function(){
+      taskbar_height = $taskbar_wrap.height(),
+      placeholder = '<div class="taskbar-placeholder" style="height:'+taskbar_height+'px"></div>';
+    }, 20);
 
     $(window).on('scroll', function () {
       var scrollTop     = $(window).scrollTop(),
