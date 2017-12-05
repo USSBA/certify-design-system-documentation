a155c9bb4d072ef43cb6d14f6c3ecb9ffe2a9083
Jared Cunha <jcunha@omb.eop.gov>
2017-12-04 19:31:37 -0500

    update for dynamic things

--- assets/javascripts/v2-components/editable-tables.js
+++ assets/javascripts/v2-components/editable-tables.js
@@ -7,16 +7,21 @@ $(document).ready(function() {
       $save_button = $editable_table.find('[id$="_save"]'),
       $add_button = $editable_table.next('[id$="_add_item"]'),
       visible_class = "is-visible";
+      var previous_values = [];
+      var itemID;
 
       var $task_panel_toggle = $('.task-panel-toggle'),
           $task_panel_content = $('.task-panel-content');
 
       var getItemID = function(e){
-        itemID = "#" + e.attr('id').replace('_edit','').replace('_delete','');
+        if (typeof e.attr('id') != "undefined") {
+          itemID = "#" + e.attr('id').replace('_edit','').replace('_delete','').replace('_save','');
+        }
       }
 
-      $edit_button.on('click', function(){
-        getItemID($(this));
+      $editable_table.on('click', '[id$="_edit"]', function(e){
+        e.stopPropagation();
+        getItemID($(e.target));
 
         // Hide the task bar
         $task_panel_toggle.attr("aria-expanded", "false");
@@ -40,8 +45,9 @@ $(document).ready(function() {
         return false;
       });
 
-      $delete_button.on('click', function(){
-        getItemID($(this));
+      $editable_table.on('click', '[id$="_delete"]', function(e){
+        e.stopPropagation();
+        getItemID($(e.target));
         var confirmed = confirm('Are you sure you want to delete this item?');
         if (confirmed) {
           $(itemID + "_data").remove();
@@ -50,7 +56,10 @@ $(document).ready(function() {
       });
 
       // Save button
-      $save_button.on('click', function(){
+      $editable_table.on('click', '[id$="_save"]', function(e){
+        e.stopPropagation();
+        getItemID($(e.target));
+        console.log(itemID);
         new_values = [];
         $(itemID + "_fields").find('input').each(function(){
           input_id = "#" + $(this).attr('id');
@@ -62,6 +71,7 @@ $(document).ready(function() {
         for (var i = 0; i < new_values.length; i++)
         {
           $(new_values[i][0] + "_text").text(new_values[i][1]);
+          console.log(new_values[i][1]);
         }
 
 
@@ -76,7 +86,8 @@ $(document).ready(function() {
 
 
       // Cancel Button
-      $cancel_button.on('click', function(){
+      $editable_table.on('click', '[id$="_cancel"]', function(e){
+        e.stopPropagation();
         // Reset the values
         for (var i = 0; i < previous_values.length; i++)
         {
@@ -93,7 +104,6 @@ $(document).ready(function() {
       });
 
       $add_button.on('click', function(){
-
         // Get some variables
         var table = "#" + $(this).attr('id').replace('_add_item',''),
             table_name = $(this).attr('id').replace('_add_item','');
@@ -112,8 +122,9 @@ $(document).ready(function() {
         // Get next ID number and number of rows needed
         var next_id = Math.max.apply(Math,row_id_arr) + 1,
             table_cols = $(table).find('thead tr th').length,
-            data_row_id = table_name + '_tr' + next_id + '_data';
-            fields_row_id = table_name + '_tr' + next_id + '_fields';
+            row_name = table_name + '_tr' + next_id,
+            data_row_id = row_name + '_data';
+            fields_row_id = row_name + '_fields';
 
         // Add new table rows
         $(table).find('tbody')
@@ -133,7 +144,7 @@ $(document).ready(function() {
         }
 
         // Add the form fieldset
-        $("#" + fields_row_id).append('<td colspan="' + table_cols + '"><ul class="sba-c-field-list"></li></td>');
+        $("#" + fields_row_id).append('<td colspan="' + table_cols + '"><ul class="sba-c-field-list"></ul><div class="sba-c-table--editable__actions"><button id="'+ row_name +'_save" class="sba-c-button">OK</button><a id="' + row_name + '_cancel"  href="#">Cancel</a></div>');
         for (var i = 0; i < table_cols - 1; i++) {
           var label_text = $(table).find('thead tr th:nth-child(' + (i + 1) + ')').text();
           var field_id = table_name + '_tr' + next_id + '_field' + (i + 1);
