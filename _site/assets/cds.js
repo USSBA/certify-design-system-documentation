ba9be945e535fc54d28e806144beeecdec517531
Tyler Bolchoz <tybolt@gmail.com>
2018-04-02 16:44:57 -0400

    wip doc upload

--- _site/assets/cds.js
+++ _site/assets/cds.js
@@ -101,9 +101,34 @@ $(document).ready(function() {
 });
 $(document).ready(function(){
   // Variables
-  var $task_panel_toggle = $('.sba-c-task-panel__toggle');
+  var $doc_upload_toggle = $('.sba-c-doc-upload__toggle'),
+      $doc_upload_content = $('.sba-c-doc-upload__content'),
+      open_class = "open",
+      // transition_class = "in-transition",
+      visible_class = "visible";
 
-  // Hooks
+  // Function for handling doc upload
+  $doc_upload_toggle.on('click', function(){
+
+    // Determine which task panel we clicked on
+    var $target = $("#" + $(this).attr("aria-controls"));
+
+    // Toggle doc uploader
+    if ($(this).attr("aria-expanded") === "false") {
+      $(this).attr("aria-expanded", "true");
+      $target.parent()
+        .addClass(open_class);
+      $target.addClass(visible_class);
+    }
+    else {
+      $(this).attr("aria-expanded", "false");
+      $target.parent()
+        .removeClass(open_class);
+      $target.removeClass(visible_class);
+    }
+
+
+  });
 
 });
 $(document).ready(function() {
