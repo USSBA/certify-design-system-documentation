1cf66ff2b08762ddedc9f12bb07e585d8a49f7c6
Tyler Bolchoz <tybolt@gmail.com>
2018-04-04 10:25:27 -0400

    wip doc upload markup examples

--- _site/assets/cds.js
+++ _site/assets/cds.js
@@ -102,31 +102,34 @@ $(document).ready(function() {
 $(document).ready(function(){
   // Variables
   var $doc_upload_toggle = $('.sba-c-doc-upload__toggle'),
-      $doc_upload_content = $('.sba-c-doc-upload__content'),
+      $doc_upload_cancel = $('.sba-c-doc-upload__cancel'),
       open_class = "open",
-      // transition_class = "in-transition",
-      visible_class = "visible";
+      visible_class = "visible",
+      hidden_class = "hidden";
 
   // Function for handling doc upload
   $doc_upload_toggle.on('click', function(){
 
     // Determine which task panel we clicked on
-    var $target = $("#" + $(this).attr("aria-controls"));
+    elem = $(this).attr("aria-controls")
+    var $toggle = $('button[aria-controls=' + elem + ']');
+    var $target = $("#" + elem);
 
     // Toggle doc uploader
-    if ($(this).attr("aria-expanded") === "false") {
-      $(this).attr("aria-expanded", "true");
-      $target.parent()
-        .addClass(open_class);
+    if ($toggle.attr("aria-expanded") === "false") {
+      $toggle.attr("aria-expanded", "true");
+      $toggle.addClass(hidden_class);
+      $target.parent().addClass(open_class);
       $target.addClass(visible_class);
     }
-    else {
-      $(this).attr("aria-expanded", "false");
-      $target.parent()
-        .removeClass(open_class);
-      $target.removeClass(visible_class);
-    }
 
+    // Toggle cancel
+    $doc_upload_cancel.on('click', function() {
+      $toggle.attr("aria-expanded", "false");
+      $toggle.removeClass(hidden_class);
+      $target.parent().removeClass(open_class);
+      $target.removeClass(visible_class);
+    })
 
   });
 
