eed3cfd6e3c81dea40663799d1827f5dd692f47f
Jared Cunha <jcunha@omb.eop.gov>
2018-05-23 11:11:23 -0400

    update deploy script and test

--- update_jekyll.sh
+++ update_jekyll.sh
@@ -1,16 +1,12 @@
-if [ ! -d "_site" ] ; then
-  git clone git@github.com:USSBA/certify-design-system-documentation.git -b gh-pages _site
-fi
-
 rm -rf _site
 bundle exec jekyll build
 cd _site
 touch .nojekyll
 git init
-git remote add origin git@github.com:USSBA/certify-design-system-documentation.git
+git remote add origin https://github.com/USSBA/certify-design-system-documentation.git
 git checkout -b gh-pages
 git add -A
 git commit -m "update site"
-git push origin gh-pages
+git push -f origin gh-pages
 cd ../
 rm -rf _site
